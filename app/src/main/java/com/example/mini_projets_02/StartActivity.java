e6aaad83a1e61379765fcde3cb1dfd495bf8a239
DESKTOP-YAS\ULTRAPC <yassinsaaden@gmail.com>
2023-06-15 22:35:54 +0100

    ex02 has been solved

--- app/src/main/java/com/example/mini_projets_02/StartActivity.java
+++ app/src/main/java/com/example/mini_projets_02/StartActivity.java
@@ -1,9 +1,15 @@
 package com.example.mini_projets_02;
 
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
@@ -18,24 +24,50 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.Objects;
+import java.util.TreeMap;
+
 public class StartActivity extends AppCompatActivity {
 
     TextView tv_startActQuote, tv_startActAuthor;
     Button btn_startActPass;
     ToggleButton tb_startActPinUnpin;
     SharedPreferences sharedPreferences;
+    Spinner spinner_startActBgColors;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
+        //region Initializing attributes
         tv_startActQuote = findViewById(R.id.tv_startActQuote);
         tv_startActAuthor = findViewById(R.id.tv_startActAuthor);
         btn_startActPass = findViewById(R.id.btn_startActPass);
         tb_startActPinUnpin = findViewById(R.id.tb_startActPinUnpin);
+        spinner_startActBgColors = findViewById(R.id.spinner_startActBgColors);
+        //endregion
+
+        //region Spinner code
+        //region Filling the spinner
+        String[] bgColors = {"Default", "LightSalmon", "Plum", "PaleGreen", "CornflowerBlue"};
+        TreeMap<String, String> bgColorsValues = new TreeMap<>();
+        bgColorsValues.put(bgColors[0], String.valueOf(((ColorDrawable) getWindow().getDecorView().getBackground()).getColor()));
+        bgColorsValues.put(bgColors[1], "#DDA0DD");
+        bgColorsValues.put(bgColors[2], "#FFA07A");
+        bgColorsValues.put(bgColors[3], "#98FB98");
+        bgColorsValues.put(bgColors[4], "#6495ED");
+        //endregion
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, bgColors);
+        spinner_startActBgColors.setAdapter(adapter);
+        //endregion
 
         sharedPreferences = getSharedPreferences("pinned-quote", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        int bgColor = sharedPreferences.getInt("bgColor", 0);
+        spinner_startActBgColors.setSelection(bgColor);
+
         String quote = sharedPreferences.getString("quote", null);
 
         if (quote == null) {
@@ -50,7 +82,7 @@ public class StartActivity extends AppCompatActivity {
         tb_startActPinUnpin.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
-                SharedPreferences.Editor editor = sharedPreferences.edit();
+
                 String quote = null;
                 String author = null;
 
@@ -69,6 +101,24 @@ public class StartActivity extends AppCompatActivity {
         btn_startActPass.setOnClickListener(v -> {
             finish();
         });
+
+
+        spinner_startActBgColors.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if (spinner_startActBgColors.getItemAtPosition(i).equals(bgColors[i])) {
+                    int color = i == 0 ? Integer.parseInt((Objects.requireNonNull(bgColorsValues.get(bgColors[i])))) : Color.parseColor(bgColorsValues.get(bgColors[i]));
+                    getWindow().getDecorView().setBackgroundColor(color);
+                    editor.putInt("bgColor", i);
+                    editor.commit();
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
     }
 
     private void getRandomQuote() {
