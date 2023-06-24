9c83228b2ad126fc4510214ff41852a67878d060
DESKTOP-YAS\ULTRAPC <yassinsaaden@gmail.com>
2023-06-24 20:26:35 +0100

    Add ContextMenu to choose the layout type of RecyclerView

--- app/src/main/java/com/example/mini_projets_02/AllFavoriteQuotesActivity.java
+++ app/src/main/java/com/example/mini_projets_02/AllFavoriteQuotesActivity.java
@@ -1,17 +1,23 @@
 package com.example.mini_projets_02;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.mini_projets_02.Adapters.FavoriteQuotesAdapter;
 
 public class AllFavoriteQuotesActivity extends AppCompatActivity {
     RecyclerView rv_allFavQuoteActList;
     FavoriteQuotesDbOpenHelper db;
+    TextView tv_allFavQuoteActChooseLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,7 @@ public class AllFavoriteQuotesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_all_favorite_quotes);
 
         rv_allFavQuoteActList = findViewById(R.id.rv_allFavQuoteActList);
+        tv_allFavQuoteActChooseLayout = findViewById(R.id.tv_allFavQuoteActChooseLayout);
 
         //region Persistence Object
 
@@ -27,8 +34,27 @@ public class AllFavoriteQuotesActivity extends AppCompatActivity {
         //endregion
 
         FavoriteQuotesAdapter adapter = new FavoriteQuotesAdapter(db.getAll());
-
+        rv_allFavQuoteActList.setLayoutManager(new GridLayoutManager(this, 2));
+        registerForContextMenu(tv_allFavQuoteActChooseLayout);
         rv_allFavQuoteActList.setAdapter(adapter);
-        rv_allFavQuoteActList.setLayoutManager(new GridLayoutManager(this , 2));
+
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        menu.setHeaderTitle("Layout type");
+        menu.add(0, v.getId(), 0, "List");
+        menu.add(0, v.getId(), 0, "Grid");
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        if (item.getTitle() == "List") {
+            rv_allFavQuoteActList.setLayoutManager(new LinearLayoutManager(this));
+        } else {
+            rv_allFavQuoteActList.setLayoutManager(new GridLayoutManager(this, 2));
+        }
+        return true;
     }
 }
